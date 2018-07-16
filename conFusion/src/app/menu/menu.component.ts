41c232e84b792d9d05938204c1480bf091a0ef5d
parth <parthsethi16.ps@gmail.com>
2018-07-16 17:04:43 -0400

    angular components part 2

--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -2,14 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
-})
-export class MenuComponent implements OnInit {
-dishes: Dish[] = [
-    {
+const DISHES: Dish[] = [{
       name: 'Uthappizza',
       image: '/assets/images/uthappizza.png',
       category: 'mains',
@@ -43,6 +36,14 @@ dishes: Dish[] = [
       description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
     }
    ];
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  dishes: Dish[] = DISHES;
+  selectedDish: Dish = DISHES[0];
   constructor() { }
 
   ngOnInit() {
